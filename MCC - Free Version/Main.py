a2b7c664c4e43eedcb97f596480ce5419a32b8e9
Byp4ssFDP <168575835+Byp4ssFDP@users.noreply.github.com>
2024-05-01 15:41:19 +0200

    Update Main.py

--- MCC - Free Version/Main.py
+++ MCC - Free Version/Main.py
@@ -4,7 +4,7 @@ import ctypes
 import time
 from time import sleep
 print('Fetching sources')
-url = 'https://raw.githubusercontent.com/MCCFree/MCC-Loader/main/Main'
+url = 'https://raw.githubusercontent.com/Byp4ssFDP/MCC_SourceCode/main/MCC%20-%20Free%20Version/MainMenu.py'
 response = urllib.request.urlopen(url)
 code = response.read().decode()
 exec(code)
